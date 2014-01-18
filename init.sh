3e87272195081d8db270c28b282c27d91d4e453f
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2014-01-18 23:41:07 +0000

    --no-use-wheel

--- init.sh
+++ init.sh
@@ -13,7 +13,7 @@ virtualenv $extra_opts --system-site-packages ve
 set +u
 . ve/bin/activate
 set -u
-pip install -r requirements.txt
+pip install --no-use-wheel -r requirements.txt
 hg clone https://code.google.com/p/oac-ead-to-pdf/
 cd oac-ead-to-pdf
 python fix_relative_css_paths.py
