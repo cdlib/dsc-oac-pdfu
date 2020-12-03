834aeb3300ae4ede665b1b96bb6186e6ade3d5f8
Brian Tingle <brian.tingle@ucop.edu>
2020-12-03 00:57:28 +0000

    new location of protable pypy

--- init.sh
+++ init.sh
@@ -3,11 +3,12 @@ set -eu
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # http://stackoverflow.com/questions/59895
 cd $DIR
 
+VER="7.2.0"
 
-wget https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-7.1.1-linux_x86_64-portable.tar.bz2
-sha256sum -c pypy-7.1.1-linux_x86_64-portable.tar.bz2.sha256
-tar jfx pypy-7.1.1-linux_x86_64-portable.tar.bz2
-./pypy-7.1.1-linux_x86_64-portable/bin/virtualenv-pypy ve
+wget https://github.com/squeaky-pl/portable-pypy/releases/download/pypy-${VER}/pypy-${VER}-linux_x86_64-portable.tar.bz2
+sha256sum -c pypy-${VER}-linux_x86_64-portable.tar.bz2.sha256
+tar jfx pypy-${VER}-linux_x86_64-portable.tar.bz2
+./pypy-${VER}-linux_x86_64-portable/bin/virtualenv-pypy ve
 set +u
 . ve/bin/activate
 set -u
