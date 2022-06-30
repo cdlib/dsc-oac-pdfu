05108e28b1ced7ac9781095422d9ef5d2806b231
Brian Tingle <brian.tingle@ucop.edu>
2022-06-30 16:16:08 +0000

    change to use mainline pypy

--- init.sh
+++ init.sh
@@ -3,17 +3,19 @@ set -eu
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # http://stackoverflow.com/questions/59895
 cd $DIR
 
-VER="7.2.0"
+VER="7.3.9"
+wget https://downloads.python.org/pypy/pypy2.7-v${VER}-linux64.tar.bz2
 
-wget https://github.com/squeaky-pl/portable-pypy/releases/download/pypy-${VER}/pypy-${VER}-linux_x86_64-portable.tar.bz2
-sha256sum -c pypy-${VER}-linux_x86_64-portable.tar.bz2.sha256
-tar jfx pypy-${VER}-linux_x86_64-portable.tar.bz2
-./pypy-${VER}-linux_x86_64-portable/bin/virtualenv-pypy ve
+sha256sum -c pypy2.7-v${VER}-linux64.tar.bz2.sha256
+tar jfx pypy2.7-v${VER}-linux64.tar.bz2
+
+pip3 install virtualenv
+virtualenv -p pypy2.7-v${VER}-linux64/bin/python ve
 set +u
 . ve/bin/activate
 set -u
 unset PYTHON_INSTALL_LAYOUT
 pip install -r requirements.txt
-git clone https://github.com/mredar/oac-ead-to-pdf.git
+git clone https://github.com/cdlib/dsc-oac-ead-to-pdf.git oac-ead-to-pdf
 cd oac-ead-to-pdf
 python fix_relative_css_paths.py
